36c8b46f14fb5a2cf17eb45ef3f2ff3b59f0142d
evi-org <133345975+evi-org@users.noreply.github.com>
2024-02-22 23:21:53 +0530

    create api route

--- api/index.js
+++ api/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
 dotenv.config();
 
-
-mongoose.connect(process.env.MONGO).then(() =>
-{
+mongoose.connect(process.env.MONGO).then(() => {
   console.log('connected to MongoDB');
-
 }).catch((err) => {
   console.log(err);
 });
-const app =express();
-app.listen(3000, () =>{
+
+const app = express();
+app.listen(3000, () => {
   console.log('server listening on port 3000');
-})
+});
+
+app.use('/api/user', userRoutes);
